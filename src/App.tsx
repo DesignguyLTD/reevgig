0a76b9be5357f9e8b809abe8b95b16cdec879dcd
seyifunmi Odediran <philipoluseyi@gmail.com>
2024-05-13 18:55:38 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://res.cloudinary.com/do5wu6ikf/image/upload/v1715619760/Reev/reev_nu0qvs.png' className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
