72117cb101463c742c81a6ef9fa19c0de7abf3dd
Mathetes041 <matthewonuoha41@gmail.com>
2024-08-19 20:46:51 +0100

    accounting settings sizing fixes

--- src/Pages/Dashboard/settings/account settings/accountSettings.tsx
+++ src/Pages/Dashboard/settings/account settings/accountSettings.tsx
@@ -62,138 +62,138 @@ const AccountSettings: React.FC = () => {
 					<p>Account Settings</p>
 				</div>
 			</div>
-			<div className={styles.profileDetails}>
-				<form action="#">
-					<div className={styles.imageSection}>
-						<input
-							type="file"
-							accept="image/*"
-							ref={fileInputRef}
-							style={{ display: 'none' }}
-							onChange={({ target: { files } }) => {
-								if (files && files[0]) {
-									setFileName(files[0].name);
-									setImage(URL.createObjectURL(files[0]));
-								}
-							}}
-						/>
-						<div className={styles.imageborder}>
-							{image && <img src={image} alt={fileName} className={styles.image} />}
-							<span className={styles.imageUploaderCtn}>
-								<img src={upload} alt="Upload" onClick={handleImageClick} />
-							</span>
-						</div>
-					</div>
 
-					<div className={styles.displayName}>
-						<label>Display Name</label>
-						<Input
-							value={displayName}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDisplayName(e.target.value)}
-							isTextArea={false}
-							disabled={true}
-							placeholder='others will see this name'
-						/>
-					</div>
-					<div className={styles.names}>
-						<span className={styles.firstName}>
-							<label>First Name</label>
-							<Input
-								value={firstName}
-								onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
-								isTextArea={false}
-								size={'small'}
-								type={"text"}
-							/>
-						</span>
-						<span className={styles.lastName}>
-							<label>Last Name</label>
-							<Input
-								value={lastName}
-								onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
-								isTextArea={false}
-								type={"text"}
-							/>
+			<form action="#">
+				<div className={styles.imageSection}>
+					<input
+						type="file"
+						accept="image/*"
+						ref={fileInputRef}
+						style={{ display: 'none' }}
+						onChange={({ target: { files } }) => {
+							if (files && files[0]) {
+								setFileName(files[0].name);
+								setImage(URL.createObjectURL(files[0]));
+							}
+						}}
+					/>
+					<div className={styles.imageborder}>
+						{image && <img src={image} alt={fileName} className={styles.image} />}
+						<span className={styles.imageUploaderCtn}>
+							<img src={upload} alt="Upload" onClick={handleImageClick} />
 						</span>
 					</div>
-					<div className={styles.mail}>
-						<label>Work email address</label>
+				</div>
+
+				<div className={styles.displayName}>
+					<label>Display Name</label>
+					<Input
+						value={displayName}
+						onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDisplayName(e.target.value)}
+						isTextArea={false}
+						disabled={true}
+						placeholder='others will see this name'
+					/>
+				</div>
+				<div className={styles.names}>
+					<span className={styles.firstName}>
+						<label>First Name</label>
 						<Input
-							value={email}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+							value={firstName}
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
 							isTextArea={false}
-							type={"email"}
+							size={'small'}
+							type={"text"}
 						/>
-					</div>
-					<div className={styles.password}>
-						<label>Password</label>
+					</span>
+					<span className={styles.lastName}>
+						<label>Last Name</label>
 						<Input
-							value={password}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-							placeholder="Password 15 or more characters"
+							value={lastName}
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
 							isTextArea={false}
-							type="password"
-							size='small'
+							type={"text"}
 						/>
-					</div>
-					<div className={styles.country}>
+					</span>
+				</div>
+				<div className={styles.mail}>
+					<label>Work email address</label>
+					<Input
+						value={email}
+						onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+						isTextArea={false}
+						type={"email"}
+					/>
+				</div>
+				<div className={styles.password}>
+					<label>Password</label>
+					<Input
+						value={password}
+						onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+						placeholder="Password 15 or more characters"
+						isTextArea={false}
+						type="password"
+						size='small'
+					/>
+				</div>
+				<div className={styles.country}>
+					<Dropdown
+						label='Country'
+						onChange={({ value }) => setCountry(value)}
+						errorMessage="Country must be selected"
+						options={countryOptions}
+						defaultText={country === "" ? "Select a country" : country}
+					/>
+				</div>
+
+				<div className={styles.stateCity}>
+					<div className={styles.state}>
 						<Dropdown
-							label='Country'
-							onChange={({ value }) => setCountry(value)}
-							errorMessage="Country must be selected"
-							options={countryOptions}
-							defaultText={country === "" ? "Select a country" : country}
+							label='State'
+							onChange={({ value }) => setState(value)}
+							errorMessage="State must be selected"
+							options={stateOptions}
+							defaultText={state === "" ? "Select a state" : state}
 						/>
 					</div>
-
-					<div className={styles.stateCity}>
-						<div className={styles.state}>
-							<Dropdown
-								label='State'
-								onChange={({ value }) => setState(value)}
-								errorMessage="State must be selected"
-								options={stateOptions}
-								defaultText={state === "" ? "Select a state" : state}
-							/>
-						</div>
-						<div className={styles.city}>
-							<Dropdown
-								label='City'
-								onChange={({ value }) => setCity(value)}
-								errorMessage="City must be selected"
-								options={cityOptions}
-								defaultText={city === "" ? "Select a city" : city}
-							/>
-						</div>
-					</div>
-
-					<div className={styles.phone}>
+					<div className={styles.city}>
 						<Dropdown
-							label='Office/Work Contact Number*'
-							onChange={({ value }) => setPhoneCode(value)}
-							options={phoneOptions}
-							defaultText={country ? `+ ${Country.getAllCountries().filter((fetchedCountry) => fetchedCountry.isoCode === country)[0].phonecode.replace(/\+/g, "")}` : phoneCode === "" ? "+234" : phoneCode}
-						/>
-						<Input
-							value={telephone}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTelephone(e.target.value)}
-							placeholder="Phone Number"
-							isTextArea={false}
-							type={"tel"}
+							label='City'
+							onChange={({ value }) => setCity(value)}
+							errorMessage="City must be selected"
+							options={cityOptions}
+							defaultText={city === "" ? "Select a city" : city}
 						/>
 					</div>
-					<div className={styles.btnCtn}>
-						<Button
-							label='Save Changes'
-							BorderColor='black'
-							icon={false}
-							onClick={() => setDisplayName(`${firstName} ${lastName}`)}
-						/>
-					</div>
+				</div>
 
-				</form>
+				<div className={styles.phone}>
+					<Dropdown
+						label='Office/Work Contact Number*'
+						onChange={({ value }) => setPhoneCode(value)}
+						options={phoneOptions}
+						defaultText={country ? `+ ${Country.getAllCountries().filter((fetchedCountry) => fetchedCountry.isoCode === country)[0].phonecode.replace(/\+/g, "")}` : phoneCode === "" ? "+234" : phoneCode}
+					/>
+					<Input
+						value={telephone}
+						onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTelephone(e.target.value)}
+						placeholder="Phone Number"
+						isTextArea={false}
+						type={"tel"}
+					/>
+				</div>
+				<div className={styles.btnCtn}>
+					<Button
+						label='Save Changes'
+						BorderColor='black'
+						icon={false}
+						onClick={() => setDisplayName(`${firstName} ${lastName}`)}
+					/>
+				</div>
+			</form>
+			<div className={styles.passwordCtn}>
+				<h2>Change password</h2>
 				<div className={styles.passwordChange}>
-					<h4>Change password</h4>
 					<div className={styles.crtPassword}>
 						<label>Current password</label>
 						<Input
@@ -221,14 +221,14 @@ const AccountSettings: React.FC = () => {
 							type={"password"}
 						/>
 					</div>
-					<div className={styles.btnCtn}>
-						<Button
-							label='Save Changes'
-							BorderColor='black'
-							icon={false}
-							onClick={() => setDisplayName(`${firstName} ${lastName}`)}
-						/>
-					</div>
+				</div>
+				<div className={styles.btnCtn}>
+					<Button
+						label='Save Changes'
+						BorderColor='black'
+						icon={false}
+						onClick={() => setDisplayName(`${firstName} ${lastName}`)}
+					/>
 				</div>
 			</div>
 		</div>
