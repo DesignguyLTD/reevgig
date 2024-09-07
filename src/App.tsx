fb915393cf208f5d74e68f621fba1ef1c0cb6632
seyifunmi Odediran <philipoluseyi@gmail.com>
2024-09-07 12:04:50 +0100

    nav bar set up

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import SignUp from "./Pages/Onboarding/SignUp/signUp";
 import Login from "./Pages/Onboarding/login/login";
 import ResetPassword from "./Pages/Onboarding/resetPassword/resetPassword";
 import OnBoarding from "./Pages/Onboarding/onboarding/onBoarding";
+import Sidebar from "./stories/SideBar/sideBar";
 
 
 
@@ -16,17 +17,22 @@ function App() {
 
     return (
         <div className="App">
-            <Header auth={true}/>
+            {/**/}
+            {/*<Sidebar/>*/}
 
             <Router basename="/">
                 <Routes>
                     <Route path="/"  element={
-                        <h1>
-                            <header className="App-header">
-                            <img src='https://res.cloudinary.com/do5wu6ikf/image/upload/v1715619760/Reev/reev_nu0qvs.png'
-                                 className="App-logo" alt="logo"/>
-                            </header>
-                        </h1>}
+                        <>
+                            <Header auth={true}/>
+                            <h1>
+                                <header className="App-header">
+                                    <img src='https://res.cloudinary.com/do5wu6ikf/image/upload/v1715619760/Reev/reev_nu0qvs.png'
+                                         className="App-logo" alt="logo"/>
+                                </header>
+                            </h1>
+                        </>
+                        }
                     />
                     <Route path="/signup" element={<SignUp/>}/>
                     <Route path="/login" element={<Login/>}/>
