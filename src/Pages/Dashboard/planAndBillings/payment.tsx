7aa2cd0c105a8d23c24542fcfc0956e8031a2cde
Mathetes041 <matthewonuoha41@gmail.com>
2024-09-07 16:38:36 +0100

    fetching icons from cloudinary

---
+++ src/Pages/Dashboard/planAndBillings/payment.tsx
@@ -0,0 +1,38 @@
+// import Radio from "../../../stories/Radio/radio";
+// import React from "react";
+// import styles from "./payment.module.css";
+
+// const Payment = () => {
+//   return (
+//     <div className={styles.Ctn}>
+//       <div className={styles.topCtn}>
+//         <div className={styles.addMoneyCtn}>
+//           <div className={styles.levelUpIcon}>
+//             {/* <img src={levelUp} alt="level up icon" /> */}
+//           </div>
+//           <div>
+//             <p>+ Add Money</p>
+//           </div>
+//         </div>
+//         <div className={styles.billingMethod}>
+//           <h5>Billing Method</h5>
+//           <p>You haven't set up any billing methods yet. Add a method so you can hire when you're ready.</p>
+//         </div>
+//       </div>
+
+//       <div className={styles.btmCtn}>
+//         {/* <img src={cancel} alt="cancel icon" /> */}
+//         <h1>Payment</h1>
+//         <div>
+//           <p>Pay With:</p>
+//           <div>
+//             <Radio label="card" id="card"/>
+//             <Radio label="local bank deposit" id="local bank"/>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Payment
